5e234b3f20004f4b7e702d78337bd0c2a640bbad
EmiBaston <156468034+EmiBaston@users.noreply.github.com>
2025-09-22 20:20:32 -0300

    Algunas Validaciones de Tipo de pago y PAgo

--- ObligatorioP2/ObligatorioP2/Pago.cs
+++ ObligatorioP2/ObligatorioP2/Pago.cs
@@ -2,5 +2,75 @@
 {
     internal class Pago
     {
+        public int Id { get; set; }
+        public static int UId {get; set;} = 0;
+        public MetodosPago MetodosPago { get; set; }
+        public Usuario UsuarioAsociado { get; set; }
+        public TipoGasto TipoGasto { get; set; }
+        public string Descripcion { get; set; }
+        public TipoPago TipoPago { get; set; }
+        public InstanciaPago InstanciaPago { get; set; }
+        public double MontoFinal { get; set; }
+
+        public Pago()
+        {
+            Id = UId;
+        }
+
+        public Pago(MetodosPago metodosPago, Usuario usuarioAsociado, TipoGasto tipoGasto, string descripcion, TipoPago tipoPago, InstanciaPago instanciaPago, double montoFinal)
+        {
+            MetodosPago = metodosPago;
+            UsuarioAsociado = usuarioAsociado;
+            TipoGasto = tipoGasto;
+            Descripcion = descripcion;
+            TipoPago = tipoPago;
+            InstanciaPago = instanciaPago;
+            MontoFinal = montoFinal;
+            Validar();
+        }
+
+        public void Validar()
+        {
+            ValidarDescripcion();
+           ValidarMetodosPago();
+           ValidarTipoPago();
+        }
+        
+      
+
+        public void ValidarTipoPago()
+        {
+            if (TipoPago != TipoPago.Recurrente && TipoPago != TipoPago.Unico)
+            {
+                throw new Exception("Tipo pago invalido");
+            }
+        }
+
+        public void ValidarMetodosPago()
+        {
+            if (MetodosPago != MetodosPago.Credito && MetodosPago != MetodosPago.Debito &&
+                MetodosPago != MetodosPago.Efectivo)
+            {
+                throw new Exception("Metodos pago invalido");
+            }
+        }
+
+        public void ValidarDescripcion()
+        {
+            if (string.IsNullOrEmpty(Descripcion))
+            {
+                throw new Exception("La descripcion de puede estar vacia");
+            }
+        }
+        
+        
+        
+      
+        
+        
+        public override string ToString()
+        {
+            return Descripcion;
+        }
     }
 }
--- ObligatorioP2/ObligatorioP2/TipoGasto.cs
+++ ObligatorioP2/ObligatorioP2/TipoGasto.cs
@@ -2,5 +2,50 @@
 {
     public class TipoGasto
     {
+        public int Id { get; set; }
+        public static int UId {get; set;} = 0;
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+
+        public TipoGasto()
+        {
+            Id = UId;
+        }
+
+        public TipoGasto(string nombre, string descripcion)
+        {
+            Id = UId;
+            Nombre = nombre;
+            Descripcion = descripcion;
+            Validar();
+        }
+
+        public void Validar()
+        {
+            ValidarNombre();
+            ValidarDescripcion();
+        }
+
+        public void ValidarNombre()
+        {
+            if (string.IsNullOrEmpty(Nombre))
+            {
+                throw new Exception("El nombre no puede estar vacio");
+            }
+            
+        }
+
+        public void ValidarDescripcion()
+        {
+            if (string.IsNullOrEmpty(Descripcion))
+            {
+                throw new Exception("La descripcion no puede estar vacia");
+            }
+        }
+
+        public override string ToString()
+        {
+            return Nombre ;
+        }
     }
 }
