7ab9544bf05b36ee4df1ed627b10a567da7b9ac4
Franco Beloqui <francobeloqui9@gmail.com>
2025-10-02 23:51:01 -0300

    Update Sistema.cs

--- ObligatorioP2/ObligatorioP2/Sistema.cs
+++ ObligatorioP2/ObligatorioP2/Sistema.cs
@@ -13,6 +13,7 @@ namespace ObligatorioP2
         private List<Equipo> Equipos;
         private List<TipoGasto> TipoGastos;
         private static Sistema Instancia;
+
         public Sistema()
         {
             Usuarios = new List<Usuario>();
@@ -28,5 +29,86 @@ namespace ObligatorioP2
             }
             return Instancia;
         }
+        public void AgregarUsuario(Usuario usuario)
+        {
+            try
+            {
+                if (usuario == null)
+                {
+                    throw new Exception("El usuario no puede ser nulo.");
+                }
+                if (Usuarios.Contains(usuario))
+                {
+                    throw new Exception("El usuario ya existe.");
+                }
+                usuario.Validar();
+
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error al validar el usuario: " + ex.Message);
+            }
+            Usuarios.Add(usuario);
+        }
+        public void AgregarEquipo(Equipo equipo)
+        {
+            try
+            {
+                if (equipo == null)
+                {
+                    throw new Exception("El equipo no puede ser nulo.");
+                }
+                if (Equipos.Contains(equipo))
+                {
+                    throw new Exception("El equipo ya existe.");
+                }
+                equipo.Validar();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error al validar el equipo: " + ex.Message);
+            }
+            Equipos.Add(equipo);
+        }
+        public void AgregarTipoGasto(TipoGasto tipoGasto)
+        {
+            try
+            {
+                if (tipoGasto == null)
+                {
+                    throw new Exception("El tipo de gasto no puede ser nulo.");
+                }
+                if (TipoGastos.Contains(tipoGasto))
+                {
+                    throw new Exception("El tipo de gasto ya existe.");
+                }
+                tipoGasto.Validar();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error al validar el tipo de gasto: " + ex.Message);
+            }
+            TipoGastos.Add(tipoGasto);
+        }
+        public void AgregarPago(Pago pago)
+        {
+            try
+            {
+                if (pago == null)
+                {
+                    throw new Exception("El pago no puede ser nulo.");
+                }
+                if (Pagos.Contains(pago))
+                {
+                    throw new Exception("El pago ya existe.");
+                }
+                pago.Validar();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error al validar el pago: " + ex.Message);
+            }
+            Pagos.Add(pago);
+        }
+        }
     }
-}
