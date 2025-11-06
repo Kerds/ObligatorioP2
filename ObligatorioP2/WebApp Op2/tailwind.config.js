7b541f9c4635f7631bda58dddaa341683c952fad
Franco Beloqui <francobeloqui9@gmail.com>
2025-11-06 13:04:57 -0300

    Andando TailwindCSS

---
+++ ObligatorioP2/WebApp Op2/tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: [
+    "./Views/**/*.cshtml",
+    "./Pages/**/*.cshtml",
+    "./**/*.cshtml",
+    "./src/**/*.css"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
