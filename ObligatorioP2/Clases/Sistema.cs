f2dd2baba50e2f39d03d434e3a13c9a81aed5e01
Franco Beloqui <francobeloqui9@gmail.com>
2025-11-11 20:53:08 -0300

    Update Sistema.cs

--- ObligatorioP2/Clases/Sistema.cs
+++ ObligatorioP2/Clases/Sistema.cs
@@ -431,7 +431,25 @@ public void PrecargaPagos()
 
         public Usuario ObtenerUsuario(string email, string contrasena)
         {
-            if (string.IsNullOrWhiteSpace(email) || string.IsNullOrWhiteSpace(contrasena))
+            if (string.IsNullOrWhiteSpace(email))
+            {
+                throw new Exception("El email y la contraseña no pueden estar vacíos.");
+
+            }
+            if (!email.Contains("@") || !email.Contains("."))
+            {
+                throw new Exception("El email ingresado no es válido.");
+            }
+            if (email.Length < 5)
+            {
+                throw new Exception("El email debe tener al menos 5 caracteres.");
+
+            }
+            if (!email.Contains("laEmpresa"))
+            {
+                throw new Exception("El email debe pertenecer a la empresa (debe contener 'laEmpresa').");
+            }
+            if ( string.IsNullOrWhiteSpace(contrasena))
             {
                 throw new Exception("El email y la contraseña no pueden estar vacíos.");
             }
