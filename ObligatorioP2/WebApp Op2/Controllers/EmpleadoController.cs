43a6970a1cfbcac1732e694ae133d2d7a5b5b246
Franco Beloqui <francobeloqui9@gmail.com>
2025-11-11 21:04:21 -0300

    Entrada a controlador

--- ObligatorioP2/WebApp Op2/Controllers/EmpleadoController.cs
+++ ObligatorioP2/WebApp Op2/Controllers/EmpleadoController.cs
@@ -1,11 +1,22 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Clases.Usuarios;
+using Microsoft.AspNetCore.Mvc;
+using ObligatorioP2;
+using WebApp_Op2.Filters;
 
 namespace WebApp_Op2.Controllers
 {
     public class EmpleadoController : Controller
     {
-        public IActionResult Index()
+        Sistema sistema = Sistema.GetSistema();
+        [LogActionFilter]
+        [RolActionFilter()]
+        public IActionResult Perfil()
         {
+            string userLogged = HttpContext.Session.GetString("usuario");
+
+            Console.WriteLine("Entré al perfil del empleado");
+            Usuario usuario = sistema.GetUsuarioPorEmail(userLogged);
+
             return View();
         }
     }
