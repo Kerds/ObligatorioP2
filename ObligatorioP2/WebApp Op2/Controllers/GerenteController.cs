1cd18b47a45bb30a3af535e3f651ca2ad104ba89
Franco Beloqui <francobeloqui9@gmail.com>
2025-11-15 19:11:22 -0300

    Update GerenteController.cs

--- ObligatorioP2/WebApp Op2/Controllers/GerenteController.cs
+++ ObligatorioP2/WebApp Op2/Controllers/GerenteController.cs
@@ -82,7 +82,8 @@ namespace WebApp_Op2.Controllers
             {
                 pagosEquipo = sistema.GetPagosMiembrosEquipo(usuario.GetNombreEquipo(), usuario);
                 ViewBag.MisPagos = sistema.GetPagosUsuario(usuario);
-              
+                ViewBag.PagosEquipo = pagosEquipo;
+
             }
             catch (Exception e)
             {
